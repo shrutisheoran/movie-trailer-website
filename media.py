1f336296d58072ed3ff83189378db8504957ab87
Shruti Sheoran <shruti.sheoran03@gmail.com>
2018-01-19 19:27:40 +0530

    Correct parameter name

--- media.py
+++ media.py
@@ -5,8 +5,8 @@ class Movie():
     # This class provides movie related information.
     VALID_RATINGS = ["G", "VG", "P", "VP"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_yout
-                 ube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
